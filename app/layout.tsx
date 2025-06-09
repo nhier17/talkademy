12e3a0bb9bd7975dac8e117a55893269a4944976
Abraham <abrahamnhier97@gmail.com>
2025-06-09 10:44:39 +0300

    initial setup

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Bricolage_Grotesque } from "next/font/google";
+import "./globals.css";
+
+const bricolage = Bricolage_Grotesque({
+  variable: "--font-bricolage",
+  subsets: ["latin"],
+});
+
+
+export const metadata: Metadata = {
+  title: "Talkademy",
+  description: "Real-time AI teaching platform",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${bricolage.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
