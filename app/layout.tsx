dd67cbee7dbb8c0b70a4982c8cc62199f80d1c91
Abraham <abrahamnhier97@gmail.com>
2025-06-09 17:08:28 +0300

    feat: navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Navbar from "@/components/ui/nav/Navbar";
 
 const bricolage = Bricolage_Grotesque({
   variable: "--font-bricolage",
@@ -25,6 +26,7 @@ export default function RootLayout({
         className={`${bricolage.variable} antialiased`}
       >
         <ClerkProvider appearance={{ variables: { colorPrimary: '#fe5933' }}}>
+          <Navbar />
           {children}
         </ClerkProvider>
       </body>
---
+++ components/ui/nav/Navbar.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import Image from "next/image";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import NavItems from "./NavItems";
+
+
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <Link href="/">
+            <div className="flex items-center gap-2.5 cursor-pointer">
+                <Image 
+                src="/images/logo.svg" 
+                alt="Logo" 
+                width={46} 
+                height={46} 
+                />
+            </div>
+            </Link>
+            <div className="flex items-center gap-8">
+                <NavItems />        
+                <SignedOut>
+                    <SignInButton>
+                        <button className="btn-sign-in">
+                            Sign In
+                        </button>
+                    </SignInButton>
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
+            </div>
+        </nav>
+    )
+}
