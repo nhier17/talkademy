5cbfec4e57ec41a8ed95227d90e127680e4c5b68
Abraham <abrahamnhier97@gmail.com>
2025-06-16 11:11:06 +0300

    managing subscriptions

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,17 +1,42 @@
 import CompanionForm from "@/components/features/CompanionForm";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import { newCompanionPermission } from "@/lib/actions/companion.actions";
+import Image from "next/image";
+import Link from "next/link";
 
 export default async function NewCompanionPage() {
     const { userId } = await auth();
     
     if (!userId) return redirect("/sign-in");
+    
+    const canCreateCompanion = await newCompanionPermission();
+    
   return (
     <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center flex flex-col gap-4">
+      {canCreateCompanion ? (
         <article className="w-full gap-4 flex flex-col">
             <h1>Companion Builder</h1>
             <CompanionForm />
         </article>
+      ) : (
+        <article className="companion-limit">
+        <Image 
+            src="/images/limit.svg" 
+            alt="Companion limit reached" 
+            width={360} 
+            height={230} 
+        />
+        <div className="cta-badge">
+            Upgrade your plan
+        </div>
+        <h1>You've Reached Your Limit</h1>
+        <p>You've reached your companion limit. Upgrade to create more companions and premium features.</p>
+        <Link href="/subscription" className="btn-primary w-full justify-center" >
+            Upgrade My Plan
+        </Link>
+    </article>
+      )}
     </main>
   )
 }
--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { auth } from "@clerk/nextjs/server";
-import { createSupabaseClient } from "@/lib/supabase";
+import { supabase } from "@/lib/supabase";
 import { revalidatePath } from "next/cache";
 
 //create a companion
@@ -9,7 +9,6 @@ export const createCompanion = async (formData: CreateCompanion) => {
     const { userId: author } = await auth();
     if (!author) throw new Error("Unauthorized");
 
-    const supabase = createSupabaseClient();
     const { data, error } = await supabase
     .from("companions")
     .insert({
@@ -23,8 +22,6 @@ export const createCompanion = async (formData: CreateCompanion) => {
 
 //get all companions
 export const getCompanions = async ({ limit = 10, page = 1, subject, topic }: GetAllCompanions) => {
-    const supabase = createSupabaseClient();
-
     let query = supabase.from("companions").select();
 
     if (subject && topic) {
@@ -45,7 +42,6 @@ export const getCompanions = async ({ limit = 10, page = 1, subject, topic }: Ge
 
 //get companion by id
 export const getCompanionById = async (id: string) => {
-    const supabase = createSupabaseClient();
     const { data: companion, error } = await supabase
     .from("companions")
     .select()
@@ -63,7 +59,6 @@ export const addToSessionHistory = async (companionId: string) => {
 
     if (!userId) throw new Error("Unauthorized");
 
-    const supabase = createSupabaseClient();
     const { data, error } = await supabase
     .from("session_history")
     .insert({
@@ -81,7 +76,6 @@ export const getSessionHistory = async (limit = 10) => {
 
     if (!userId) throw new Error("Unauthorized");
 
-    const supabase = createSupabaseClient();
     const { data, error } = await supabase
     .from("session_history")
     .select(`companions:companion_id (*)`)
@@ -94,7 +88,6 @@ export const getSessionHistory = async (limit = 10) => {
 
 //get user sessions
 export const getUserSessions = async (userId: string, limit = 10) => {
-    const supabase = createSupabaseClient();
     const { data, error } = await supabase
     .from("session_history")
     .select(`companions:companion_id (*)`)
@@ -108,7 +101,6 @@ export const getUserSessions = async (userId: string, limit = 10) => {
 
 //get user companions
 export const getUserCompanions = async (userId: string) => {
-    const supabase = createSupabaseClient();
     const { data, error } = await supabase
     .from("companions")
     .select()
@@ -117,4 +109,35 @@ export const getUserCompanions = async (userId: string) => {
     if (error || !data) throw new Error(error?.message || "Failed to get user companions");
     return data;
 };
+
+//companion permissions/subscription
+export const newCompanionPermission = async () => {
+    const { userId, has } = await auth();
+
+    if (!userId) throw new Error("Unauthorized");
+
+    let limit = 0;
+    if(has({ plan: 'pro' })) {
+        return true;
+    } else if(has({ feature: "3_companion_limit" })) {
+        limit = 3;
+    } else if(has({ feature: "10_companion_limit" })) {
+        limit = 10;
+    }
+
+    const { data, error } = await supabase
+    .from("companion_permissions")
+    .select('id', {count: 'exact'})
+    .eq("author", userId)
+
+    if (error || !data) throw new Error(error?.message || "Failed to create companion permission");
+    const companionCount = data?.length;
+
+    if (companionCount >= limit) {
+        return false;
+    } else {
+        return true;
+    }
+}; 
+ 
 
